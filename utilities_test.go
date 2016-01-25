d941a5840835295f0fd1d3f28f24c06455df327d
claudemiro <dimiro1@gmail.com>
2016-01-25 20:20:11 -0200

    Testing fillString

--- utilities_test.go
+++ utilities_test.go
@@ -2,6 +2,16 @@ package faker
 
 import "testing"
 
+func TestBothify(t *testing.T) {
+	s := bothify("Hello &&&")
+	assertStringRegexp(t, "Hello \\w{3}", s)
+}
+
+func TestFillString(t *testing.T) {
+	s := fillString("#", 10)
+	assertStringRegexp(t, "^[#]{10}$", s)
+}
+
 func TestNumerify(t *testing.T) {
 	s := numerify("Hello ###")
 	assertStringRegexp(t, "Hello \\d{3}", s)
@@ -10,11 +20,6 @@ func TestNumerify(t *testing.T) {
 func TestLexify(t *testing.T) {
 	s := lexify("Hello ???")
 	assertStringRegexp(t, "Hello [a-z]{3}", s)
-}
-
-func TestBothify(t *testing.T) {
-	s := bothify("Hello &&&")
-	assertStringRegexp(t, "Hello \\w{3}", s)
 }
 
 func TestHexify(t *testing.T) {
