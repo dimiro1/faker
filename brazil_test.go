6d7421640727ebf611c954e356f8b9969226024b
claudemiro <dimiro1@gmail.com>
2016-01-25 16:59:22 -0200

    The regex must include start and end symbols

--- brazil_test.go
+++ brazil_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestBrazilCPF(t *testing.T) {
 	f := NewDefaultFaker()
-	assertStringRegexp(t, "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", f.BrazilCPF())
+	assertStringRegexp(t, "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$", f.BrazilCPF())
 }
 
 func TestBrazilCNPJ(t *testing.T) {
 	f := NewDefaultFaker()
-	assertStringRegexp(t, "\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}", f.BrazilCNPJ())
+	assertStringRegexp(t, "^\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}$", f.BrazilCNPJ())
 }
