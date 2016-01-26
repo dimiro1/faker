9aa4bdcda55ac59a1646f6174734d9c2edff09fb
claudemiro <dimiro1@gmail.com>
2016-01-25 22:47:15 -0200

    Removed forgotten comment

--- brazil.go
+++ brazil.go
@@ -43,7 +43,6 @@ func (f Faker) BrazilCNPJ() string {
 		cnpj[0], cnpj[1], cnpj[2], cnpj[3], cnpj[4], cnpj[5],
 		cnpj[6], cnpj[7], cnpj[8], cnpj[9], cnpj[10], cnpj[11], v[0], v[1],
 	)
-	// return "45.294.670/0001-04"
 }
 
 // BrazilCPF Generates a valid Brazilian CPF
