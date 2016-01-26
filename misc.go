5ba0f89ea8b859bf67b86c849abed1d9d19c7a9b
claudemiro <dimiro1@gmail.com>
2016-01-25 22:44:24 -0200

    Implemented misc functions

--- misc.go
+++ misc.go
@@ -1,17 +1,32 @@
 package faker
 
+import "math/rand"
+
+// Boolean returns a random true or false
 func (f Faker) Boolean() bool {
-	return true
+	return rand.Float64() > 0.5
 }
 
+// MD5 returns a random MD5 hexdigest
+// See: https://en.wikipedia.org/wiki/MD5
 func (f Faker) MD5() string {
-	return "23434343434"
+	return hexify(fillString("*", 32))
 }
 
+// SHA1 returns a random SHA1 hexdigest
+// See: https://en.wikipedia.org/wiki/SHA-1
 func (f Faker) SHA1() string {
-	return "23434343434"
+	return hexify(fillString("*", 40))
 }
 
+// SHA256 returns a random SHA256 hexdigest
+// See: https://en.wikipedia.org/wiki/SHA-2
 func (f Faker) SHA256() string {
-	return "23434343434"
+	return hexify(fillString("*", 64))
+}
+
+// SHA512 returns a random SHA512 hexdigest
+// See: https://en.wikipedia.org/wiki/SHA-2
+func (f Faker) SHA512() string {
+	return hexify(fillString("*", 128))
 }
---
+++ misc_test.go
@@ -0,0 +1,23 @@
+package faker
+
+import "testing"
+
+func TestFakerMD5(t *testing.T) {
+	f := NewDefaultFaker()
+	assertStringRegexp(t, "^[\\d0-f]{32}$", f.MD5())
+}
+
+func TestFakerSHA1(t *testing.T) {
+	f := NewDefaultFaker()
+	assertStringRegexp(t, "^[\\d0-f]{40}$", f.SHA1())
+}
+
+func TestFakerSHA256(t *testing.T) {
+	f := NewDefaultFaker()
+	assertStringRegexp(t, "^[\\d0-f]{64}$", f.SHA256())
+}
+
+func TestFakerSHA512(t *testing.T) {
+	f := NewDefaultFaker()
+	assertStringRegexp(t, "^[\\d0-f]{128}$", f.SHA512())
+}
