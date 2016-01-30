7218c16a2e86ee0b1e4f621f682a0293b5212afb
claudemiro <dimiro1@gmail.com>
2016-01-29 23:18:09 -0200

    implemented CMYK

--- color.go
+++ color.go
@@ -2,19 +2,33 @@ package faker
 
 import "image/color"
 
-func (f Faker) HexColor() string {
-	return "HexColor"
-}
-
 // ColorName returns a name of a ColorName
 func (f Faker) ColorName() string {
 	return randomElement(colorNames)
 }
 
-func (f Faker) RGBA() color.RGBA {
-	return color.RGBA{R: 0, G: 0, B: 0, A: 1}
+// CMYK returns a valid CMYK color
+func (f Faker) CMYK() color.CMYK {
+	r := uint8(randomIntBetween(0, 255))
+	g := uint8(randomIntBetween(0, 255))
+	b := uint8(randomIntBetween(0, 255))
+
+	c, m, y, k := color.RGBToCMYK(r, g, b)
+
+	return color.CMYK{C: c, M: m, Y: y, K: k}
 }
 
-func (f Faker) CMYK() color.CMYK {
-	return color.CMYK{C: 0, M: 0, Y: 0, K: 0}
+// HexColor returns a hexadecimal color, including alpha
+func (f Faker) HexColor() string {
+	return hexify(fillString("*", 8))
+}
+
+// RGBA returns a valid RGBA color
+func (f Faker) RGBA() color.RGBA {
+	r := uint8(randomIntBetween(0, 255))
+	g := uint8(randomIntBetween(0, 255))
+	b := uint8(randomIntBetween(0, 255))
+	a := uint8(randomIntBetween(0, 255))
+
+	return color.RGBA{R: r, G: g, B: b, A: a}
 }
--- color_test.go
+++ color_test.go
@@ -7,3 +7,9 @@ func TestFakerColorName(t *testing.T) {
 	l := f.ColorName()
 	assertElementInSlice(t, l, colorNames)
 }
+
+func TestFakerHexColor(t *testing.T) {
+	f := NewDefault()
+	c := f.HexColor()
+	assertStringRegexp(t, "[0-9a-f]{8}", c)
+}
