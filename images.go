43840c2806be307ff3b69e77cda6d2f9001665c1
claudemiro <dimiro1@gmail.com>
2016-02-19 09:11:04 -0200

    implemented PlaceholderImageWithOptions

--- images.go
+++ images.go
@@ -1,10 +1,15 @@
 package faker
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/dimiro1/faker/defaults"
+)
 
 type ImageOptions struct {
 	Size   string `default:"300x300"`
-	Format string `default:"jpg"`
+	Format string `default:".jpg"`
 }
 
 func (f Faker) AvatarImage() string {
@@ -23,6 +28,31 @@ func (f Faker) PlaceholderImage() string {
 	return fmt.Sprintf("http://placehold.it/%dx%d", width, height)
 }
 
+// PlaceholderImageWithOptions returns a Placeholder image
 func (f Faker) PlaceholderImageWithOptions(o ImageOptions) string {
-	return "AvatarImageWithOptions"
+	defaults.Apply(&o)
+
+	formats := []string{".jpeg", ".jpg", ".gif", ".png"}
+	valid := false
+
+	for _, format := range formats {
+		if o.Format == format {
+			valid = true
+		}
+	}
+
+	if !valid {
+		panic(fmt.Sprintf("images: %s is an invalid format, valid formats are %s", o.Format, formats))
+	}
+
+	size := strings.Split(o.Size, "x")
+
+	if len(size) != 2 {
+		panic(fmt.Sprintf("images: %s is an invalid size, valid format is 300x300", o.Size))
+	}
+
+	width := size[0]
+	height := size[1]
+
+	return fmt.Sprintf("http://placehold.it/%sx%s%s", width, height, o.Format)
 }
--- images_test.go
+++ images_test.go
@@ -6,3 +6,37 @@ func TestFakerPlaceholderImage(t *testing.T) {
 	f := NewDefault()
 	assertStringRegexp(t, "^http://placehold.it/\\d+x\\d+$", f.PlaceholderImage())
 }
+
+func TestFakerPlaceholderImageWithOptions_with_defaults(t *testing.T) {
+	f := NewDefault()
+	assertStringRegexp(t, "^http://placehold.it/\\d+x\\d+\\.(jpeg|jpg|gif|png)$", f.PlaceholderImageWithOptions(ImageOptions{}))
+}
+
+func TestFakerPlaceholderImageWithOptions(t *testing.T) {
+	f := NewDefault()
+	assertStringRegexp(t, "^http://placehold.it/400x500.png$", f.PlaceholderImageWithOptions(ImageOptions{Size: "400x500", Format: ".png"}))
+}
+
+func TestFakerPlaceholderImageWithOptions_with_invalid_format(t *testing.T) {
+	f := NewDefault()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Should panic with wrong Format argument")
+		}
+	}()
+
+	f.PlaceholderImageWithOptions(ImageOptions{Format: ".asas"})
+}
+
+func TestFakerPlaceholderImageWithOptions_with_invalid_size(t *testing.T) {
+	f := NewDefault()
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Should panic with wrong Size argument")
+		}
+	}()
+
+	f.PlaceholderImageWithOptions(ImageOptions{Size: "asasacdcd"})
+}
